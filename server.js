510e3e7a31280e82f9cbb956e779df0644a2ec18
Dani Carter <carte470@d.umn.edu>
2018-02-26 19:49:55 -0600

    array calc work

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require("./routing/apiRoutes")(app);
