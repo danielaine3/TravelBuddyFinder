76b957fa59cea03de15d2e5a580e036433fddb81
Dani Carter <carte470@d.umn.edu>
2018-02-27 18:17:40 -0600

    compare function work moved to apiRoutes file

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -6,7 +6,34 @@ module.exports = function(app) {
 	});
 
 	app.post("/api/friends", function(req, res) {
-		friendsData.push(req.body);
-		res.json(friendsData);
+
+		//Empty array to push matched friend into
+		var bestFriend = {
+			name: "",
+			photo: "",
+			friendDifference: 1000
+		};
+
+		var userData = req.body;
+		var userScores = userData.scores;
+		var totalDiff = 0;
+
+		for (var i = 0; i < friendsData.length; i++) {
+			totalDiff = 0
+
+
+			for (var j= 0; j < friendsData[i].scores[j]; j++) {
+				totalDiff += Math.abs(userScores[j]-friendsData[i].scores[j]);
+
+				if (totalDifference <= bestFriend.friendDifference) {
+					bestFriend.name = friendsData[i].name;
+					bestFriend.photo = friendsData[i].photo;
+					bestFriend.friendDifference = totalDiff;
+				}
+			}
+		}
+
+		friendsData.push(userData);
+		res.json(bestFriend);
 	});
 };
