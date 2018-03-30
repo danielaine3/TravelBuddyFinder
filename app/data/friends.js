175a234534acdb616886d638c3fc554205488836
Dani Carter <carte470@d.umn.edu>
2018-03-30 15:57:30 -0500

    adding code comments

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,51 +1,24 @@
+//DATA
+//Below will be data for all for all of the travel buddies.
+//Initially we set it equal to a "dummy" buddy. 
+//But it could be an empty array as well
 var friendsData = [
 	{
 		"name": "Kim Kardashian",
 		"photo": "https://pbs.twimg.com/profile_images/941034987440128000/52bwGYtY_400x400.jpg",
-		"scores": [
-		3,
-		5,
-		4,
-		3,
-		5,
-		4,
-		1,
-		5,
-		2,
-		5
-		]
+		"scores": [3,5,4,3,5,4,1,5,2,5]
 	},
 	{
 		"name": "Kylie Jenner",
 		"photo": "https://metrouk2.files.wordpress.com/2018/02/pri_68451998-e1519285242449.jpg?w=748&h=462&crop=1",
-		"scores": [
-		"3",
-		"2",
-		"4",
-		"4",
-		"2",
-		"2",
-		"2",
-		"4",
-		"3",
-		"3"
-		]
+		"scores": ["3","2","4","4","2","2","2","4","3","3"]
 	},
 	{
 		"name": "Khloe Kardashian",
 		"photo": "http://images.eonline.com/eol_images/Entire_Site/201804/rs_600x600-180104093157-Khloepreg600-.jpg?fit=inside|300:300&output-quality=100",
-		"scores": [
-		"5",
-		"5",
-		"5",
-		"5",
-		"2",
-		"2",
-		"3",
-		"4",
-		"3",
-		"3"
-		]
+		"scores": ["5","5","5","5","2","2","3","4","3","3"]
 	}
 ];	
+
+//Export the array making it accessible to other files.
 module.exports = friendsData;
--- server.js
+++ server.js
@@ -2,17 +2,26 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
 // Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 3000;
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:"application/vnd.api+json"}));
 app.use(express.static('app/public'));
+
+//ROUTER
+//The below points our server to sereis of "route" files.
+//These routes give out server a "map" of how to respond when users visit or request data from various URLs
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
+//Starts the server to begin listening
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
+
